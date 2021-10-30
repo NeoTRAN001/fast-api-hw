ea2a6e6afeea7f26005563a6225ada0966b4a5a7
NeoTRAN001 <neo_tran@outlook.com>
2021-10-30 18:13:40 -0500

    feat: params

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Optional
+from fastapi.datastructures import Default
 from pydantic import BaseModel
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -23,3 +24,10 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=2, max_length=50),
+    age: str = Query(...)
+):
+    return {name: age}
